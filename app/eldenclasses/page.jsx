3d5bb63baebf8c9c62c8d9374f0fbcc4abf5dd76
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-10-17 15:28:34 -0300

    feat: style armor finalized

--- app/eldenclasses/page.jsx
+++ app/eldenclasses/page.jsx
@@ -21,43 +21,43 @@ export default function eldenClasses() {
             }
         };
         classesFetch();
-    },[]);
+    }, []);
     return (
         <div className={styles.bckg}>
             <Header />
             <h3 className={styles.centralizedText}>Classes</h3>
             <div className={styles.dualdiv}>
                 {
-                dadosApi ? (
-                    dadosApi.data.map((classes) => (
-                    <div key={classes.id} className={styles.redcard}>
-                        <h1 className={styles.centralizedText}>{classes.name}</h1>
-                        <img src={classes.image} alt={classes.name} className={styles.image}/>
-                        <div className={styles.statusarea}>
-                            {
-                                classes.stats.map((status) => (
-                                    <div key={status.id} >
-  <p className={styles.centralizedText}>{status.level}</p>
+                    dadosApi ? (
+                        dadosApi.data.map((classes) => (
+                            <div key={classes.id} className={styles.redcard}>
+                                <h1 className={styles.centralizedText}>{classes.name}</h1>
+                                <img src={classes.image} alt={classes.name} className={styles.image} />
+                                <div className={styles.statusarea}>
+                                    {
+                                        classes.stats.map((status) => (
+                                            <div key={status.id} >
+                                                <p className={styles.centralizedText}>{status.level}</p>
 
 
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                    ))
-                ) : (
-                    <ProgressBar
-                        height="80"
-                        width="80"
-                        ariaLabel="progress-bar-loading"
-                        wrapperStyle={{}}
-                        wrapperClass="progress-bar-wrapper"
-                        borderColor='#42b883'
-                        barColor='#51E5FF'
-                    />
-                )
-            }
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <ProgressBar
+                            height="80"
+                            width="80"
+                            ariaLabel="progress-bar-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="progress-bar-wrapper"
+                            borderColor='#42b883'
+                            barColor='#51E5FF'
+                        />
+                    )
+                }
             </div>
             <Footer />
         </div>
