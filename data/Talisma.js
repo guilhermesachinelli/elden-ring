dfa9aa3ca3f6e39e276bdb94235ab5cdf7a03f34
manuumqm <manuelamachado@aluno.senai.br>
2023-10-17 15:26:25 -0300

    api chefes

---
+++ data/Talisma.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const URL_API = 'https://eldenring.fanapis.com/api/talismans'
+
+const talisma = async () => {
+    try {
+        const resposta = await axios.get(URL_API);
+        return resposta.data;
+    } catch (error) {
+        throw error;
+    }
+}
+export default talisma
