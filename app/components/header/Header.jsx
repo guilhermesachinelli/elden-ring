cc724d9fefedb99545fe043112968bf6fb70ac1c
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-10-24 16:05:44 -0300

    feat: header

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,22 +1,36 @@
+'use client'
+import React, { useState } from 'react';
+import styles from './Header.module.css';
 import Image from 'next/image';
 import NavLink from '../navlink/NavLink';
-import styles from './Header.module.css';
-
 
 const Header = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <div className={styles.header}>
-            <div className={styles.dropdown}>
-                <Image className={styles.dropbtn} src={'/ER_golden_order.webp'} alt={'Menu'} width={74} height={64} ></Image>
-                <div className={styles.dropdown_content}>
-                    <NavLink rota={'/'} text={'Home'} className={styles.navlink} />
-                    <NavLink rota={'/eldenclasses'} text={'Clases'} className={styles.navlink} />
-                    <NavLink rota={'/armor'} text={'Armaduras'} className={styles.navlink} />
-                    <NavLink rota={'/chefes'} text={'Bosses'} className={styles.navlink} />
-                </div>
+        <div className={`${styles.sideMenu} ${isMenuOpen ? styles.open : ''}`}>
+            <div className={styles.menu}>
+            <div className={styles.icon} onClick={toggleMenu}>
+                <Image src="/ER_golden_order.webp" alt="Elden Ring" width={84} height={74} />
             </div>
+            </div>
+            <ul className={styles.menuItems}>
+                <NavLink rota="/" text={"Home"} icon={"faHouse"}/>
+                <NavLink rota="/eldenclasses" text={"Classes"} icon={"faScroll"}/>
+                <NavLink rota="/locations" text={"Locais"} icon={"faMapLocationDot"}/>
+                <NavLink rota="/chefes" text={"Bosses"} icon={"faChessKing"}/>
+                <NavLink rota="/armas" text={"Armas"} icon={"faSkullCrossbones"}/>
+                <NavLink rota="/armor" text={"Armaduras"} icon={"faShirt"}/>
+                <NavLink rota="/talisma" text={"Talismã"} icon={"faRing"}/>
+
+
+            </ul>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
