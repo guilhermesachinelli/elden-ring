31de576925d12e22bc9f31ce9bf7454f99c3260b
Nicolly <123502051+santni@users.noreply.github.com>
2023-10-26 08:34:01 -0300

    feat: alt footer page 5

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -2,7 +2,6 @@
 import Image from 'next/image';
 import styles from './Footer.module.css';
 import NavImg from '../navimg/NavImg';
-import NavLinkFooter from '../navlinkfooter/NavLinkFooter';
 
 
 const Footer = () => {
@@ -10,18 +9,6 @@ const Footer = () => {
         <div className={styles.footer}>
             
             <Image src="/ER_golden_order.webp" alt="Logo" width={84} height={74} />
-            <div className={styles.menu}>
-            <ul>
-                <NavLinkFooter rota="/" text={"Home"} icon={"faHouse"}/>
-                <NavLinkFooter rota="/eldenclasses" text={"Classes"} icon={"faScroll"}/>
-                <NavLinkFooter rota="/locations" text={"Locais"} icon={"faMapLocationDot"}/>
-                <NavLinkFooter rota="/chefes" text={"Bosses"} icon={"faChessKing"}/>
-                <NavLinkFooter rota="/armas" text={"Armas"} icon={"faSkullCrossbones"}/>
-                <NavLinkFooter rota="/armor" text={"Armaduras"} icon={"faShirt"}/>
-                <NavLinkFooter rota="/talisma" text={"Talismã"} icon={"faRing"}/>
-            </ul>
-            </div>
-            <div className={styles.direitos}>
             <p>Todos os Direitos Reservados © 2023.</p>
             
             <div className={styles.socialmedia}>
@@ -30,7 +17,6 @@ const Footer = () => {
                 <NavImg rota="https://twitter.com" img="/x.png" />
             </div>
             </div>
-        </div>
     )
 }
 
