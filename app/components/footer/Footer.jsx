38919e12db5b71b74d357185da06f7f85dbd68e4
Nicolly <123502051+santni@users.noreply.github.com>
2023-10-26 08:30:55 -0300

    feat: alt footer page 4

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import styles from './Footer.module.css';
 import NavImg from '../navimg/NavImg';
-import NavLink from '../navlink/NavLink';
+import NavLinkFooter from '../navlinkfooter/NavLinkFooter';
 
 
 const Footer = () => {
@@ -12,13 +12,13 @@ const Footer = () => {
             <Image src="/ER_golden_order.webp" alt="Logo" width={84} height={74} />
             <div className={styles.menu}>
             <ul>
-                <NavLink rota="/" text={"Home"} icon={"faHouse"}/>
-                <NavLink rota="/eldenclasses" text={"Classes"} icon={"faScroll"}/>
-                <NavLink rota="/locations" text={"Locais"} icon={"faMapLocationDot"}/>
-                <NavLink rota="/chefes" text={"Bosses"} icon={"faChessKing"}/>
-                <NavLink rota="/armas" text={"Armas"} icon={"faSkullCrossbones"}/>
-                <NavLink rota="/armor" text={"Armaduras"} icon={"faShirt"}/>
-                <NavLink rota="/talisma" text={"Talismã"} icon={"faRing"}/>
+                <NavLinkFooter rota="/" text={"Home"} icon={"faHouse"}/>
+                <NavLinkFooter rota="/eldenclasses" text={"Classes"} icon={"faScroll"}/>
+                <NavLinkFooter rota="/locations" text={"Locais"} icon={"faMapLocationDot"}/>
+                <NavLinkFooter rota="/chefes" text={"Bosses"} icon={"faChessKing"}/>
+                <NavLinkFooter rota="/armas" text={"Armas"} icon={"faSkullCrossbones"}/>
+                <NavLinkFooter rota="/armor" text={"Armaduras"} icon={"faShirt"}/>
+                <NavLinkFooter rota="/talisma" text={"Talismã"} icon={"faRing"}/>
             </ul>
             </div>
             <div className={styles.direitos}>
