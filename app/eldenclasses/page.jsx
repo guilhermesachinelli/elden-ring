ee6f159e1b7af6d53492132b1605032134736002
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-10-26 10:51:08 -0300

    feat: paginacao

--- app/eldenclasses/page.jsx
+++ app/eldenclasses/page.jsx
@@ -7,6 +7,8 @@ import classesRing, { changePage } from '@/data/classesRing';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import { ProgressBar } from 'react-loader-spinner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons'
 
 export default function eldenClasses() {
     const [dadosApi, setDadosApi] = useState('');
@@ -21,8 +23,8 @@ export default function eldenClasses() {
         if (pageNumber <= 0) {
             return
         } else {
-        setPageNumber(pageNumber - 1)
-        changePage(pageNumber)
+            setPageNumber(pageNumber - 1)
+            changePage(pageNumber)
         }
     }
 
@@ -37,102 +39,106 @@ export default function eldenClasses() {
         const classesFetch = async () => {
             try {
                 const dados = await classesRing();
-                if(!ignore){
-                setDadosApi(dados)
+                if (!ignore) {
+                    setDadosApi(dados)
                 }
-            }  catch (error) {
-            throw error;
-        }
-    };
-    classesFetch();
-    return () => { ignore = true }
-}, [pageNumber]);
-return (
-    <div className={styles.bckg}>
-        <Header />
-        <h1 className={styles.centralizedText}>Classes</h1>
-        <div className={styles.btnarea}>
-            <button className={styles.btn} onClick={prevPage}>Prev</button>
-            <select className={styles.select} onChange={selectPage}>
-                <option value="0">Page 1</option>
-                <option value="1">Page 2</option>
-                <option value="2">Page 3</option>
-                <option value="3">Page 4</option>
-            </select>
-            <button className={styles.btn} onClick={nextPage}>Next</button>
-        </div>
-        <div className={styles.dualdiv}>
-
-            {
-
-                dadosApi ? (
-                    dadosApi.data.map((classes) => (
-                        <div key={classes.id} className={styles.redcard}>
-                            <h1 className={styles.centralizedText}>{classes.name}</h1>
-                            <img src={classes.image} alt={classes.name} className={styles.image} />
-                            <div className={styles.statusarea}>
-                                <table className={styles.table}>
-                                    <tbody className={styles.tbody}>
-                                        <tr className={styles.tr}>
-                                            <td className={styles.whitetext}>
-                                                Level: {classes.stats.level}
-                                            </td>
-                                        </tr>
-                                        <tr className={styles.tr}>
-                                            <td className={styles.whitetext}>
-                                                Vigor: {classes.stats.vigor}
-                                            </td>
-                                            <td className={styles.whitetext}>
-                                                Destreza: {classes.stats.dexterity}
-                                            </td>
-                                        </tr>
-                                        <tr className={styles.tr}>
-                                            <td className={styles.whitetext}>
-                                                Mente: {classes.stats.mind}
-                                            </td>
-                                            <td className={styles.whitetext}>
-                                                Inteligencia: {classes.stats.intelligence}
-                                            </td>
-                                        </tr>
-                                        <tr className={styles.tr}>
-                                            <td className={styles.whitetext}>
-                                                Resistência : {classes.stats.endurance}
-                                            </td>
-                                            <td className={styles.whitetext}>
-                                                Fé : {classes.stats.faith}
-                                            </td>
-                                        </tr>
-                                        <tr className={styles.tr}>
-                                            <td className={styles.whitetext}>
-                                                Força  : {classes.stats.strength}
-                                            </td>
-                                            <td className={styles.whitetext}>
-                                                Arcano : {classes.stats.arcane}
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <p className={styles.nomalCentralizedText}>{classes.description}</p>
-
-                        </div>
-
-                    ))
-                ) : (
-                    <ProgressBar
-                        height="80"
-                        width="80"
-                        ariaLabel="progress-bar-loading"
-                        wrapperStyle={{}}
-                        wrapperClass="progress-bar-wrapper"
-                        borderColor='#42b883'
-                        barColor='#51E5FF'
-                    />
-                )
+            } catch (error) {
+                throw error;
             }
-        </div>
-        <Footer />
-    </div>
+        };
+        classesFetch();
+        return () => { ignore = true }
+    }, [pageNumber]);
+    return (
+        <div className={styles.bckg}>
+            <Header />
+            <h1 className={styles.centralizedText}>Classes</h1>
+            <div className={styles.btnarea}>
+                <button className={styles.btn} onClick={prevPage}>
+                    <FontAwesomeIcon icon={faCaretLeft} />
+                </button>
+                <select className={styles.select} onChange={selectPage}>
+                    <option value="0">Page 1</option>
+                    <option value="1">Page 2</option>
+                    <option value="2">Page 3</option>
+                    <option value="3">Page 4</option>
+                </select>
+                <button className={styles.btn} onClick={nextPage}>
+                    <FontAwesomeIcon icon={faCaretRight} />
+                </button>
+            </div>
+            <div className={styles.dualdiv}>
 
-)
+                {
+
+                    dadosApi ? (
+                        dadosApi.data.map((classes) => (
+                            <div key={classes.id} className={styles.redcard}>
+                                <h1 className={styles.centralizedText}>{classes.name}</h1>
+                                <img src={classes.image} alt={classes.name} className={styles.image} />
+                                <div className={styles.statusarea}>
+                                    <table className={styles.table}>
+                                        <tbody className={styles.tbody}>
+                                            <tr className={styles.tr}>
+                                                <td className={styles.whitetext}>
+                                                    Level: {classes.stats.level}
+                                                </td>
+                                            </tr>
+                                            <tr className={styles.tr}>
+                                                <td className={styles.whitetext}>
+                                                    Vigor: {classes.stats.vigor}
+                                                </td>
+                                                <td className={styles.whitetext}>
+                                                    Destreza: {classes.stats.dexterity}
+                                                </td>
+                                            </tr>
+                                            <tr className={styles.tr}>
+                                                <td className={styles.whitetext}>
+                                                    Mente: {classes.stats.mind}
+                                                </td>
+                                                <td className={styles.whitetext}>
+                                                    Inteligencia: {classes.stats.intelligence}
+                                                </td>
+                                            </tr>
+                                            <tr className={styles.tr}>
+                                                <td className={styles.whitetext}>
+                                                    Resistência : {classes.stats.endurance}
+                                                </td>
+                                                <td className={styles.whitetext}>
+                                                    Fé : {classes.stats.faith}
+                                                </td>
+                                            </tr>
+                                            <tr className={styles.tr}>
+                                                <td className={styles.whitetext}>
+                                                    Força  : {classes.stats.strength}
+                                                </td>
+                                                <td className={styles.whitetext}>
+                                                    Arcano : {classes.stats.arcane}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <p className={styles.nomalCentralizedText}>{classes.description}</p>
+
+                            </div>
+
+                        ))
+                    ) : (
+                        <ProgressBar
+                            height="80"
+                            width="80"
+                            ariaLabel="progress-bar-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="progress-bar-wrapper"
+                            borderColor='#42b883'
+                            barColor='#51E5FF'
+                        />
+                    )
+                }
+            </div>
+            <Footer />
+        </div>
+
+    )
 }
