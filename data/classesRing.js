3a5fedb22c5eafb86b6b908223bb28f462aaf751
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-10-26 09:12:20 -0300

    feat: paginação

--- data/classesRing.js
+++ data/classesRing.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+let URL = "https://eldenring.fanapis.com/api/classes?limit=4&page=0";
+
+export function changePage(page) {
+    URL = `https://eldenring.fanapis.com/api/classes?limit=4&page=${page}`
+    return URL;
+}
+
 const classesRing = async () => {
     try {
-        const resposta = await axios.get("https://eldenring.fanapis.com/api/classes");
+        const resposta = await axios.get(URL);
         return resposta.data
     } catch (error) {
         throw error;
